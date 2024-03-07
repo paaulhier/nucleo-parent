e81f4499bcf06d1e4fc6f6306f898a5579070322
Paul <kpaul7811@gmail.com>
2024-03-07 23:33:12 +0100

    try to implement modules into spring in a nicer way - not successful .-.

--- nucleo-core/core-application/application-spring/src/main/java/de/keeeks/nucleo/application/spring/listener/ContextRefreshedEventListener.java
+++ nucleo-core/core-application/application-spring/src/main/java/de/keeeks/nucleo/application/spring/listener/ContextRefreshedEventListener.java
@@ -1,12 +1,15 @@
 package de.keeeks.nucleo.application.spring.listener;
 
+import de.keeeks.nucleo.application.spring.SpringModule;
 import de.keeeks.nucleo.core.api.ServiceRegistry;
 import de.keeeks.nucleo.core.application.command.Console;
 import de.keeeks.nucleo.core.application.command.command.CommandRegistry;
 import de.keeeks.nucleo.core.loader.ModuleLoader;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
@@ -14,13 +17,20 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ContextRefreshedEventListener implements ApplicationListener<ContextRefreshedEvent> {
     private final ModuleLoader moduleLoader;
+    private boolean modulesLoaded = false;
 
     @Override
     public void onApplicationEvent(@NotNull ContextRefreshedEvent event) {
-        ServiceRegistry.service(Console.class).startLineReading();
-        ServiceRegistry.service(CommandRegistry.class).registerCommands();
-        moduleLoader.loadModulesFromFolder();
-        moduleLoader.loadModules();
-        moduleLoader.enableModules();
+        SpringModule.context(
+                (AnnotationConfigServletWebServerApplicationContext) event.getApplicationContext()
+        );
+        if (!modulesLoaded) {
+            ServiceRegistry.service(Console.class).startLineReading();
+            ServiceRegistry.service(CommandRegistry.class).registerCommands();
+            moduleLoader.loadModulesFromFolder();
+            moduleLoader.loadModules();
+            moduleLoader.enableModules();
+            modulesLoaded = true;
+        }
     }
 }
