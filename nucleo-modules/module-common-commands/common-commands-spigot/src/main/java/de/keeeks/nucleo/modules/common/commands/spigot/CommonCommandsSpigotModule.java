24d4ac24b75230a3d8147d299e33a87dbc28c855
Paul <kpaul7811@gmail.com>
2024-03-18 21:38:42 +0100

    implemented the FlyCommand - https://jira.keeeks.dev/browse/NUC-25

--- nucleo-modules/module-common-commands/common-commands-spigot/src/main/java/de/keeeks/nucleo/modules/common/commands/spigot/CommonCommandsSpigotModule.java
+++ nucleo-modules/module-common-commands/common-commands-spigot/src/main/java/de/keeeks/nucleo/modules/common/commands/spigot/CommonCommandsSpigotModule.java
@@ -4,6 +4,7 @@ import de.keeeks.nucleo.core.api.ModuleDescription;
 import de.keeeks.nucleo.core.spigot.module.SpigotModule;
 import de.keeeks.nucleo.modules.common.commands.api.translation.CommonCommandsTranslationRegistry;
 import de.keeeks.nucleo.modules.common.commands.spigot.commands.BetterPasteLocationCommand;
+import de.keeeks.nucleo.modules.common.commands.spigot.commands.FlyCommand;
 import de.keeeks.nucleo.modules.common.commands.spigot.commands.ModulesCommand;
 import de.keeeks.nucleo.modules.common.commands.spigot.commands.UptimeCommand;
 import de.keeeks.nucleo.modules.translation.global.TranslationRegistry;
@@ -26,7 +27,8 @@ public class CommonCommandsSpigotModule extends SpigotModule {
         registerCommands(
                 new BetterPasteLocationCommand(),
                 new ModulesCommand(),
-                new UptimeCommand(plugin())
+                new UptimeCommand(plugin()),
+                new FlyCommand()
         );
     }
 }
---
+++ nucleo-modules/module-common-commands/common-commands-spigot/src/main/java/de/keeeks/nucleo/modules/common/commands/spigot/commands/FlyCommand.java
@@ -0,0 +1,79 @@
+package de.keeeks.nucleo.modules.common.commands.spigot.commands;
+
+import de.keeeks.nucleo.core.api.Module;
+import de.keeeks.nucleo.modules.common.commands.spigot.CommonCommandsSpigotModule;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+import revxrsal.commands.annotation.Command;
+import revxrsal.commands.annotation.DefaultFor;
+import revxrsal.commands.annotation.Optional;
+import revxrsal.commands.bukkit.annotation.CommandPermission;
+
+@Command("fly")
+@CommandPermission("nucleo.commands.fly")
+public class FlyCommand {
+
+    @DefaultFor("fly")
+    public void flyCommand(
+            Player player,
+            @Optional String targetName
+    ) {
+        if (targetName == null || targetName.equalsIgnoreCase(player.getName())) {
+            toggleFlyForSelf(player);
+            return;
+        }
+
+        Player target = Bukkit.getPlayer(targetName);
+        if (target == null) {
+            player.sendMessage(Component.translatable("playerNotFound"));
+            return;
+        }
+
+        toggleFlyForOther(player, target);
+    }
+
+    private void toggleFlyForOther(Player player, Player target) {
+        if (toggleFlyFor(player)) {
+            player.sendMessage(Component.translatable(
+                    "commands.fly.enabled.other",
+                    Component.text(target.getName())
+            ));
+        } else {
+            player.sendMessage(Component.translatable(
+                    "commands.fly.disabled.other",
+                    Component.text(target.getName())
+            ));
+        }
+    }
+
+    private void toggleFlyForSelf(Player player) {
+        if (toggleFlyFor(player)) {
+            player.sendMessage(Component.translatable("commands.fly.enabled.self"));
+        } else {
+            player.sendMessage(Component.translatable("commands.fly.disabled.self"));
+        }
+    }
+
+    private boolean toggleFlyFor(Player player) {
+        if (player.getAllowFlight()) {
+            player.setAllowFlight(false);
+            player.setFlying(false);
+            return false;
+        } else {
+            player.setVelocity(new Vector(0, 0.0125, 0).normalize());
+            Bukkit.getScheduler().runTaskLater(
+                    Module.module(CommonCommandsSpigotModule.class).plugin(),
+                    () -> {
+                        player.setAllowFlight(true);
+                        player.setFlying(true);
+                    },
+                    2
+            );
+            return true;
+        }
+    }
+
+}
