d79094c4f8259d13c7a24bfbfbe62f79e7cfb7cf
Paul <kpaul7811@gmail.com>
2024-03-20 15:16:20 +0100

    implemented ResultSetTransformer#uuidOrNull and timestampOrNull

--- nucleo-modules/module-database/database-sql/src/main/java/de/keeeks/nucleo/modules/database/sql/statement/ResultSetTransformer.java
+++ nucleo-modules/module-database/database-sql/src/main/java/de/keeeks/nucleo/modules/database/sql/statement/ResultSetTransformer.java
@@ -1,10 +1,23 @@
 package de.keeeks.nucleo.modules.database.sql.statement;
 
+import org.checkerframework.checker.nullness.qual.Nullable;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.Instant;
+import java.util.UUID;
 
 public interface ResultSetTransformer<T> {
 
     T transform(ResultSet resultSet) throws SQLException;
 
+    @Nullable
+    default UUID uuidOrNull(ResultSet resultSet, String key) throws SQLException {
+        return resultSet.getString(key) != null ? UUID.fromString(resultSet.getString(key)) : null;
+    }
+
+    @Nullable
+    default Instant timestampOrNull(ResultSet resultSet, String key) throws SQLException {
+        return resultSet.getTimestamp(key) != null ? resultSet.getTimestamp(key).toInstant() : null;
+    }
 }
