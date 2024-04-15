3eb0665b0768ea08162282db17e92696cc1172e7
Paul <kpaul7811@gmail.com>
2024-04-15 23:31:22 +0200

    implemented ServiceRegistry#registerService(name, object)

--- nucleo-core/core-api/src/main/java/de/keeeks/nucleo/core/api/ServiceRegistry.java
+++ nucleo-core/core-api/src/main/java/de/keeeks/nucleo/core/api/ServiceRegistry.java
@@ -4,18 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public final class ServiceRegistry {
-    private static final Map<Class<?>, Object> services = new HashMap<>();
+    private static final Map<String, Object> services = new HashMap<>();
 
     public static <T> T registerService(Class<T> clazz, T service) {
-        services.put(clazz, service);
+        registerService(clazz.getName(), service);
         Class<?> serviceClass = service.getClass();
         if (serviceClass != clazz) {
-            services.put(serviceClass, service);
+            registerService(serviceClass.getName(), service);
         }
         return service;
     }
 
+    public static <T> T registerService(String name, T service) {
+        services.put(name, service);
+        return service;
+    }
+
+    public static <T> T service(String name) {
+        return (T) services.get(name);
+    }
+
     public static <T> T service(Class<T> clazz) {
-        return (T) services.get(clazz);
+        return (T) services.get(clazz.getName());
     }
 }
