709056e516ff1030db43636f5cf792b9d2dde9eb
Paul <kpaul7811@gmail.com>
2024-04-17 00:10:16 +0200

    set cloudnet-bridge as softdependency

--- nucleo-core/core-velocity/src/main/java/de/keeeks/nucleo/core/velocity/NucleoVelocityPlugin.java
+++ nucleo-core/core-velocity/src/main/java/de/keeeks/nucleo/core/velocity/NucleoVelocityPlugin.java
@@ -5,6 +5,7 @@ import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
+import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
 import de.keeeks.nucleo.core.loader.ModuleLoader;
@@ -27,7 +28,10 @@ import java.util.logging.Logger;
         id = "nucleo-velocity",
         name = "Nucleo Velocity",
         version = "1.0-SNAPSHOT",
-        description = "Nucleo Core for Velocity"
+        description = "Nucleo Core for Velocity",
+        dependencies = {
+                @Dependency(id = "cloudnet-bridge", optional = true),
+        }
 )
 public class NucleoVelocityPlugin {
     @Getter
