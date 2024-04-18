b77584554ae2a2f18f396c160e5999bfe3f3e75b
Paul <kpaul7811@gmail.com>
2024-04-18 19:18:39 +0200

    changed that the current page can't be higher than the total possible pages

--- nucleo-core/core-api/src/main/java/de/keeeks/nucleo/core/api/utils/pagination/PaginationResult.java
+++ nucleo-core/core-api/src/main/java/de/keeeks/nucleo/core/api/utils/pagination/PaginationResult.java
@@ -1,16 +1,25 @@
 package de.keeeks.nucleo.core.api.utils.pagination;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
 public record PaginationResult<T>(List<T> list, int page, int totalPages) {
-
     public static <T> PaginationResult<T> create(List<T> fullList, int page, int pageSize) {
-        int fromIndex = (page - 1) * pageSize;
-        int toIndex = Math.min(page * pageSize, fullList.size());
+        List<T> subList = processListSplitting(fullList, page, pageSize);
+        int totalPages = (int) Math.ceil((double) fullList.size() / pageSize);
+
+        if (page > totalPages) {
+            return new PaginationResult<>(
+                    processListSplitting(fullList, totalPages, pageSize),
+                    totalPages,
+                    totalPages
+            );
+        }
         return new PaginationResult<>(
-                fullList.subList(fromIndex, toIndex),
+                subList,
                 page,
-                (int) Math.ceil((double) fullList.size() / pageSize)
+                totalPages
         );
     }
 
@@ -20,5 +29,16 @@ public record PaginationResult<T>(List<T> list, int page, int totalPages) {
                 page,
                 10
         );
+    }
+
+    private static <T> @NotNull List<T> processListSplitting(List<T> fullList, int page, int pageSize) {
+        List<T> subList;
+        int fromIndex = (page - 1) * pageSize;
+        int toIndex = Math.min(page * pageSize, fullList.size());
+        subList = fullList.subList(
+                fromIndex,
+                toIndex
+        );
+        return subList;
     }
 }
