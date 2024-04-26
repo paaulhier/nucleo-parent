92ad6c88c4968916d2c692584314f28c20e49ab6
Paul <kpaul7811@gmail.com>
2024-04-26 15:41:58 +0200

    added punishment information to AltsCommand

--- nucleo-modules/module-moderation-tools/moderation-tools-velocity/src/main/java/de/keeeks/nucleo/modules/moderation/tools/velocity/commands/player/AltsCommand.java
+++ nucleo-modules/module-moderation-tools/moderation-tools-velocity/src/main/java/de/keeeks/nucleo/modules/moderation/tools/velocity/commands/player/AltsCommand.java
@@ -1,11 +1,15 @@
 package de.keeeks.nucleo.modules.moderation.tools.velocity.commands.player;
 
 import com.velocitypowered.api.proxy.Player;
+import de.keeeks.karistus.api.Punishment;
+import de.keeeks.karistus.api.PunishmentApi;
+import de.keeeks.karistus.api.PunishmentType;
 import de.keeeks.lejet.api.NameColorizer;
 import de.keeeks.nucleo.core.api.ServiceRegistry;
 import de.keeeks.nucleo.core.api.utils.pagination.PaginationResult;
 import de.keeeks.nucleo.modules.players.api.NucleoPlayer;
 import de.keeeks.nucleo.modules.players.api.PlayerService;
+import net.kyori.adventure.text.Component;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.annotation.Default;
 import revxrsal.commands.annotation.DefaultFor;
@@ -14,15 +18,16 @@ import revxrsal.commands.velocity.annotation.CommandPermission;
 
 import java.util.List;
 
-import static net.kyori.adventure.text.Component.text;
-import static net.kyori.adventure.text.Component.translatable;
+import static net.kyori.adventure.text.Component.*;
 
 @Command({"alts"})
 @CommandPermission("nucleo.commands.alts")
 public class AltsCommand {
-
     private final PlayerService playerService = ServiceRegistry.service(
             PlayerService.class
+    );
+    private final PunishmentApi punishmentApi = ServiceRegistry.service(
+            PunishmentApi.class
     );
 
     @Usage("nucleo.commands.alts.usage")
@@ -54,7 +59,6 @@ public class AltsCommand {
                 page
         );
 
-
         player.sendMessage(translatable(
                 "nucleo.commands.alts.header",
                 NameColorizer.coloredName(nucleoPlayer.uuid()),
@@ -62,10 +66,27 @@ public class AltsCommand {
                 text(paginatedPlayers.totalPages()),
                 text(paginatedPlayers.totalAmount())
         ));
+
         for (NucleoPlayer alternativeAccount : paginatedPlayers) {
+            Component banReason = punishmentApi.activePunishment(
+                    alternativeAccount.uuid(),
+                    PunishmentType.BAN
+            ).map(punishment -> {
+                System.out.println("Punishment: " + punishment.reason());
+                return punishment.reason();
+            }).orElse(translatable("nucleo.commands.alts.notBanned"));
+
+            Component muteReason = punishmentApi.activePunishment(
+                    alternativeAccount.uuid(),
+                    PunishmentType.MUTE
+            ).map(Punishment::reason).orElse(translatable("nucleo.commands.alts.notMuted"));
+
             player.sendMessage(translatable(
                     "nucleo.commands.alts.entry",
-                    NameColorizer.coloredName(alternativeAccount.uuid())
+                    NameColorizer.coloredName(alternativeAccount.uuid()),
+                    text(alternativeAccount.name()),
+                    banReason,
+                    muteReason
             ));
         }
     }
