d9433198334e1dc75a381d6dae2c4c5b6698a9b2
Paul <kpaul7811@gmail.com>
2024-05-21 23:09:31 +0200

    implemented HotBarItem#interactAtEntity

--- nucleo-modules/module-inventory/src/main/java/de/keeeks/nucleo/modules/inventory/hotbar/item/HotBarItem.java
+++ nucleo-modules/module-inventory/src/main/java/de/keeeks/nucleo/modules/inventory/hotbar/item/HotBarItem.java
@@ -1,14 +1,23 @@
 package de.keeeks.nucleo.modules.inventory.hotbar.item;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import xyz.xenondevs.invui.item.ItemProvider;
 
 @Getter
-@RequiredArgsConstructor
 public abstract class HotBarItem {
+
+    private boolean allowLeftClick = false;
+
+    public HotBarItem() {
+    }
+
+    public HotBarItem(boolean allowLeftClick) {
+        this.allowLeftClick = allowLeftClick;
+    }
 
     public abstract ItemProvider itemProvider();
 
@@ -17,4 +26,11 @@ public abstract class HotBarItem {
             int slot,
             PlayerInteractEvent event
     );
+
+    public void interactAtEntity(
+            Player player,
+            int slot,
+            Entity entity
+    ) {
+    }
 }
--- nucleo-modules/module-inventory/src/main/java/de/keeeks/nucleo/modules/inventory/hotbar/listener/PlayerHotBarInteractListener.java
+++ nucleo-modules/module-inventory/src/main/java/de/keeeks/nucleo/modules/inventory/hotbar/listener/PlayerHotBarInteractListener.java
@@ -5,9 +5,14 @@ import de.keeeks.nucleo.modules.inventory.hotbar.PlayerHotBar;
 import de.keeeks.nucleo.modules.inventory.hotbar.PlayerHotBarService;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 public class PlayerHotBarInteractListener implements Listener {
 
@@ -19,14 +24,44 @@ public class PlayerHotBarInteractListener implements Listener {
     public void handleInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         ItemStack item = event.getItem();
-        if (!event.getAction().isRightClick() || item == null) return;
+        if (item == null) return;
 
         PlayerHotBar playerHotBar = playerHotBarService.hotBar(player);
         if (playerHotBar == null) return;
         int heldItemSlot = player.getInventory().getHeldItemSlot();
 
+        playerHotBar.item(heldItemSlot).ifPresent(hotBarItem -> {
+            if (!hotBarItem.allowLeftClick() && event.getAction().isLeftClick()) return;
+            hotBarItem.interact(player, heldItemSlot, event);
+        });
+    }
+
+    @EventHandler
+    public void handleInteractAtEntity(PlayerInteractAtEntityEvent event) {
+        Player player = event.getPlayer();
+        PlayerInventory inventory = player.getInventory();
+        if (inventory.getItem(EquipmentSlot.HAND).getType().isAir()) return;
+
+        PlayerHotBar playerHotBar = playerHotBarService.hotBar(player);
+        if (playerHotBar == null) return;
+        int heldItemSlot = inventory.getHeldItemSlot();
+
         playerHotBar.item(heldItemSlot).ifPresent(
-                hotBarItem -> hotBarItem.interact(player, heldItemSlot, event)
+                hotBarItem -> hotBarItem.interactAtEntity(player, heldItemSlot, event.getRightClicked())
         );
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void handleDamageByEntity(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Player player) {
+            PlayerHotBar playerHotBar = playerHotBarService.hotBar(player);
+            if (playerHotBar == null) return;
+            int heldItemSlot = player.getInventory().getHeldItemSlot();
+
+            playerHotBar.item(heldItemSlot).ifPresent(hotBarItem -> {
+                if (!hotBarItem.allowLeftClick()) return;
+                hotBarItem.interactAtEntity(player, heldItemSlot, event.getEntity());
+            });
+        }
     }
 }
