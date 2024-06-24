311fd6a255d30d693d5ecfecc3167b500e779d2b
Paul <kpaul7811@gmail.com>
2024-06-24 20:27:25 +0200

    added some documentation to the automatic message api

--- nucleo-modules/module-automessage/automessage-api/src/main/java/de/keeeks/nucleo/modules/automessage/api/AutomaticMessage.java
+++ nucleo-modules/module-automessage/automessage-api/src/main/java/de/keeeks/nucleo/modules/automessage/api/AutomaticMessage.java
@@ -7,23 +7,67 @@ import java.util.UUID;
 
 public interface AutomaticMessage {
 
+    /**
+     * Returns the id of the message
+     * @return the id of the message
+     */
     int id();
 
+    /**
+     * Returns the message of the message
+     * @return the message of the message
+     */
     Component message();
 
+    /**
+     * Sets the message of the message
+     * @param message the new message of the message
+     * @return the message
+     */
     AutomaticMessage message(Component message);
 
+    /**
+     * Changes the state of the message to the given state
+     * @param state the new state of the message
+     * @return the message
+     */
     AutomaticMessage enabled(boolean state);
 
+    /**
+     * Returns a timestamp when the message was created
+     * @return a timestamp when the message was created
+     */
     Instant createdAt();
 
+    /**
+     * Returns the creator of the message
+     * @return the creator of the message
+     */
     UUID createdBy();
 
+    /**
+     * Returns a timestamp when the message was last updated
+     * @return a timestamp when the message was last updated
+     */
     Instant updatedAt();
 
+    /**
+     * Returns the last updater of the message
+     * @return the last updater of the message
+     */
     UUID lastUpdatedBy();
 
+    /**
+     * Returns the state of the message
+     * if true, the message is enabled
+     * @return the state of the message
+     */
     boolean enabled();
 
+    /**
+     * Updates the message with the given updater
+     * @param updater the updater of the message
+     * @return the updated message
+     */
     AutomaticMessage update(UUID updater);
 }
--- nucleo-modules/module-automessage/automessage-api/src/main/java/de/keeeks/nucleo/modules/automessage/api/AutomaticMessageApi.java
+++ nucleo-modules/module-automessage/automessage-api/src/main/java/de/keeeks/nucleo/modules/automessage/api/AutomaticMessageApi.java
@@ -10,42 +10,107 @@ public interface AutomaticMessageApi {
 
     String CHANNEL = "nucleo:automessage";
 
+    /**
+     * Reloads the messages from the database
+     */
     void reload();
 
+    /**
+     * Returns a list of all messages, regardless of their state
+     * @return a list of all messages
+     */
     List<AutomaticMessage> messages();
 
-    default List<AutomaticMessage> enabledMessages() {
-        return messages().stream().filter(AutomaticMessage::enabled).toList();
-    }
-
-    default List<AutomaticMessage> disabledMessages() {
-        return messages().stream().filter(message -> !message.enabled()).toList();
-    }
-
-    default Optional<AutomaticMessage> message(int messageId) {
-        return messages().stream().filter(message -> message.id() == messageId).findFirst();
-    }
-
+    /**
+     * Creates a new message with the given component and creator
+     * @param component the component of the message
+     * @param creator the creator of the message
+     * @return the created message
+     */
     AutomaticMessage createMessage(Component component, UUID creator);
 
+    /**
+     * Creates a new message with the given message and creator
+     * @param message the message of the message
+     * @param creator the creator of the message
+     * @return the created message
+     */
     AutomaticMessage createMessage(String message, UUID creator);
 
+    /**
+     * Deletes the message with the given id
+     * @param messageId the id of the message to delete
+     */
     void deleteMessage(int messageId);
 
+    /**
+     * Updates the message with the given id
+     * @param messageId the id of the message to update
+     * @param component the new component of the message
+     * @param updater the updater of the message
+     */
     void updateMessage(int messageId, Component component, UUID updater);
 
+    /**
+     * Updates the message with the given id
+     * @param messageId the id of the message to update
+     * @param updater the updater of the message
+     */
     void enableMessage(int messageId, UUID updater);
 
+    /**
+     * Disables the message with the given id
+     * @param messageId the id of the message to disable
+     * @param updater the updater of the message
+     */
+    void disableMessage(int messageId, UUID updater);
+
+    /**
+     * Enables the given message
+     * @param message the message to enable
+     * @param updater the updater of the message
+     */
     default void enableMessage(AutomaticMessage message, UUID updater) {
         enableMessage(message.id(), updater);
     }
 
-    void disableMessage(int messageId, UUID updater);
+    /**
+     * Returns a list of all enabled messages
+     * @return a list of all enabled messages
+     */
+    default List<AutomaticMessage> enabledMessages() {
+        return messages().stream().filter(AutomaticMessage::enabled).toList();
+    }
 
+    /**
+     * Returns a list of all disabled messages
+     * @return a list of all disabled messages
+     */
+    default List<AutomaticMessage> disabledMessages() {
+        return messages().stream().filter(message -> !message.enabled()).toList();
+    }
+
+    /**
+     * Returns an optional of the message with the given id
+     * @param messageId the id of the message
+     * @return an optional of the message with the given id
+     */
+    default Optional<AutomaticMessage> message(int messageId) {
+        return messages().stream().filter(message -> message.id() == messageId).findFirst();
+    }
+
+    /**
+     * Returns an optional of the message with the given id
+     * @param message the message
+     */
     default void disableMessage(AutomaticMessage message) {
         disableMessage(message.id(), message.lastUpdatedBy());
     }
 
+    /**
+     * Deletes the given message
+     * @param message the message to delete
+     */
     default void deleteMessage(AutomaticMessage message) {
         deleteMessage(message.id());
     }
